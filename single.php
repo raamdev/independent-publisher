54b706f9991c6c0e59229c00d2a6ef3b632f19fb
Raam Dev <raam@raamdev.com>
2013-09-08 13:09:23 -0400

    * Increase font size for Asides * Remove single.php bottom nav

--- single.php
+++ single.php
@@ -15,8 +15,6 @@ get_header(); ?>
 
 				<?php get_template_part( 'content', 'single' ); ?>
 
-				<?php publish_content_nav( 'nav-below' ); ?>
-
 				<?php
 					// If comments are open or we have at least one comment, load up the comment template
 					if ( comments_open() || '0' != get_comments_number() )
