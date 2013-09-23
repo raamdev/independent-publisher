48976a590208e6e51294932f03756def9bbba2e7
Raam Dev <raam@raamdev.com>
2013-09-23 12:47:52 -0400

    Remove unnecessary lines to improve readability in single.php

--- single.php
+++ single.php
@@ -22,39 +22,25 @@ get_header(); ?>
 				?>
 
 				<?php if( is_single() && function_exists('wp_related_posts') ) : ?>
-
 					<div id="further-reading">
-
 						<?php do_action('erp-show-related-posts', array('title'=>'Further Reading', 'num_to_display'=>5, 'no_rp_text'=>'No Related Posts Found')); ?>
-
 					</div>
-
 				<?php endif; ?>
 
 				<?php if( get_the_tag_list() ) : ?>
-
 					<div id="taglist">
-
 						<?php echo get_the_tag_list('<ul class="taglist"><li class="taglist-title">Related Content by Tag</li><li>','</li><li>','</li></ul>'); ?>
-
 					</div>
-
 				<?php endif; ?>
 
 				<?php if ( have_comments() ) : ?>
-
 					<?php if ( count($wp_query->comments_by_type['pings'])) { ?>
-
 						<ul class="pinglist">
 							<li class="pinglist-title">Readers who shared this</li>
-
 							<?php wp_list_comments('type=pings&callback=independent_publisher_ping'); ?>
-
 							<li class="pinglist-title">Thank you!</li>
 						</ul>
-
 					<?php } // end if ( count($wp_query->comments_by_type['pings']))?>
-
 				<?php endif; // end have_comments()?>
 
 			<?php endwhile; // end of the loop. ?>
