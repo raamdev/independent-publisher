1664063071d7aa7d51dd9536367298a8699f195f
Raam Dev <raam@raamdev.com>
2013-10-04 13:02:37 -0400

    * Center author info at top of page on browsers < 1200px * Auto-hide the address bar on mobile browsers (tested on iOS 6) * Add initial-scale and maximum-scale to viewport to improve mobile experience

--- header.php
+++ header.php
@@ -11,14 +11,14 @@
 <html <?php language_attributes(); ?>>
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>" />
-	<meta name="viewport" content="width=device-width" />
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
 	<title><?php wp_title( '-', true, 'right' ); ?></title>
 	<link rel="profile" href="http://gmpg.org/xfn/11" />
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
 	<!--[if lt IE 9]>
 	<script src="<?php echo get_template_directory_uri(); ?>/js/html5.js" type="text/javascript"></script>
 	<![endif]-->
-
+	<script src="<?php echo get_template_directory_uri(); ?>/js/hide-address-bar.js" type="text/javascript"></script>
 	<?php wp_head(); ?>
 </head>
 
---
+++ js/hide-address-bar.js
@@ -0,0 +1,7 @@
+/**
+ * Automatically hide address bar on mobile browsers to improve reading experience.
+ * The page must finish *fully* loading before this happens.
+ */
+jQuery(document).ready(function(){
+	window.scrollTo(0, 1);
+});
