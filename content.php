6712cc40c2d5c58a5c158113b2503591b72efcee
Raam Dev <raam@raamdev.com>
2013-10-16 15:37:32 -0400

    Improvements to Blog, Archive, and Search pages 	* Decrease entry title font size 	* Link excerpt to post permalink 	* Add word count to entry meta for standard format posts 	* Add independent_publisher_post_word_count() function for generating word count

--- content.php
+++ content.php
@@ -6,9 +6,6 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<?php if ( has_post_thumbnail() ) : ?>
-		<a href="<?php the_permalink(); ?>" title="<?php echo esc_attr( sprintf( __( 'Permalink to %s', 'independent_publisher' ), the_title_attribute( 'echo=0' ) ) ); ?>" rel="bookmark"><?php the_post_thumbnail( array( 700, 700 ) ); ?></a>
-	<?php endif; ?>
 	<header class="entry-header">
 		<h1 class="entry-title">
 			<a href="<?php the_permalink(); ?>" title="<?php echo esc_attr( sprintf( __( 'Permalink to %s', 'independent_publisher' ), the_title_attribute( 'echo=0' ) ) ); ?>" rel="bookmark"><?php the_title(); ?></a>
@@ -33,6 +30,11 @@
 				<?php $content = preg_replace( '!<sup\s+id="fnref.*?">.*?</sup>!is', '', $content ); ?>
 
 				<a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php echo $content; ?></a>
+
+			<?php elseif ( ! get_post_format() ) : // Standard post format ?>
+
+				<a style="text-decoration: none; color: inherit;" href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_excerpt(); ?></a>
+
 			<?php else : ?>
 
 				<?php the_content( __( 'Continue reading <span class="meta-nav">&rarr;</span>', 'independent_publisher' ) ); ?>
@@ -47,11 +49,15 @@
 			<?php independent_publisher_posted_author_cats() ?>
 		<?php endif; // End if 'post' == get_post_type() ?>
 
-		<?php if ( ! post_password_required() && ( comments_open() || '0' != get_comments_number() ) ) : ?>
+		<?php if ( ! get_post_format() ) : // Only show word count on standard post format ?>
+			<span class="sep"> | </span>
+			<span><?php echo independent_publisher_post_word_count() . ' Words '; ?></span>
+		<?php endif; ?>
+
+		<?php if ( ! post_password_required() && comments_open() ) : ?>
 			<span class="sep"> | </span>
 			<span class="comments-link"><?php comments_popup_link( __( 'Comment', 'independent_publisher' ), __( '1 Comment', 'independent_publisher' ), __( '% Comments', 'independent_publisher' ) ); ?></span>
 		<?php endif; ?>
-
 		<?php edit_post_link( __( 'Edit', 'independent_publisher' ), '<span class="sep"> | </span><span class="edit-link">', '</span>' ); ?>
 	</footer>
 	<!-- .entry-meta -->
