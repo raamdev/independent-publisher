b8edebb002f871c7c8199a4c4277c79903663267
Raam Dev <raam@raamdev.com>
2013-10-21 15:21:52 -0400

    Cleaning up.

--- inc/customizer.php
+++ inc/customizer.php
@@ -7,8 +7,6 @@
  */
 
 /**
- * Add postMessage support for site title and description for the Theme Customizer.
- *
  * @param WP_Customize_Manager $wp_customize Theme Customizer object.
  */
 function independent_publisher_customize_register( $wp_customize ) {
