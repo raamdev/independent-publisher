23d99e64a437a4c91d98d965c1be5cf59eaecbc7
Raam Dev <raam@raamdev.com>
2014-01-01 11:49:20 -0500

    Reintroduce Excerpt options to Customizer. Fixes #40.
    * New Excerpts section in Customizer
    * Excerpts options available again after user demand :)
    * Excerpt options now use radio buttons to improve usability
    * Rewrote Customizer to use new IndependentPublisher_Customize class

--- content.php
+++ content.php
@@ -6,6 +6,12 @@
 ?>
 <article id="post-<?php the_ID(); ?>" <?php independent_publisher_post_classes(); ?>>
 	<header class="entry-header">
+		<?php if ( independent_publisher_show_full_content_first_post() && ( independent_publisher_is_very_first_standard_post() && is_home() && ! is_sticky() ) ) : ?>
+			<h2 class="entry-title-meta">
+				<span class="entry-title-meta-author"><?php independent_publisher_posted_author() ?></span> in <?php echo independent_publisher_post_categories( '', TRUE ); ?>
+				<?php do_action('independent_publisher_entry_title_meta', $separator = ' | ' ); ?>
+			</h2>
+		<?php endif; ?>
 		<h1 class="entry-title">
 			<a href="<?php the_permalink(); ?>" title="<?php echo esc_attr( sprintf( __( 'Permalink to %s', 'independent_publisher' ), the_title_attribute( 'echo=0' ) ) ); ?>" rel="bookmark"><?php the_title(); ?></a>
 		</h1>
@@ -25,12 +31,27 @@
 
 				<a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php echo $content; ?></a>
 
-			<?php else : ?>
+			<?php elseif ( independent_publisher_show_full_content_first_post() && independent_publisher_is_very_first_standard_post() ) : ?>
 
 				<?php if ( has_post_thumbnail() ) : ?>
 					<?php the_post_thumbnail( array( 700, 700 ) ); ?>
 				<?php endif; ?>
+				<?php the_content( __( 'Continue reading <span class="meta-nav">&rarr;</span>', 'independent_publisher' ) ); ?>
+				<?php wp_link_pages( array( 'before' => '<div class="page-links">' . __( 'Pages:', 'independent_publisher' ), 'after' => '</div>' ) ); ?>
 
+			<?php
+			elseif ( ! get_post_format() && ! is_sticky() &&
+			         ( independent_publisher_use_post_excerpts() || independent_publisher_use_enhanced_excerpts() )
+			) : // Standard post format
+					?>
+
+					<a style="text-decoration: none; color: inherit;" href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_excerpt(); ?></a>
+
+				<?php
+			else : ?>
+				<?php if ( has_post_thumbnail() ) : ?>
+					<?php the_post_thumbnail( array( 700, 700 ) ); ?>
+				<?php endif; ?>
 				<?php the_content( __( 'Continue reading <span class="meta-nav">&rarr;</span>', 'independent_publisher' ) ); ?>
 				<?php wp_link_pages( array( 'before' => '<div class="page-links">' . __( 'Pages:', 'independent_publisher' ), 'after' => '</div>' ) ); ?>
 
@@ -39,21 +60,25 @@
 	<?php endif; ?>
 
 	<footer class="entry-meta">
-		<?php if ( 'post' == get_post_type() ) : // Hide category and tag text for pages on Search ?>
+		<?php if ( 'post' == get_post_type() && independent_publisher_is_not_first_post_full_content() ) : // Hide category and tag text for pages on Search ?>
 			<?php independent_publisher_posted_author_cats() ?>
 		<?php endif; // End if 'post' == get_post_type() ?>
 
-		<?php if ( false === get_post_format() && independent_publisher_show_post_word_count() ) : // Only show word count on standard post format ?>
+		<?php if ( false === get_post_format() && independent_publisher_show_post_word_count() && independent_publisher_is_not_first_post_full_content() ) : // Only show word count on standard post format ?>
 			<?php echo independent_publisher_get_post_word_count() ?>
 		<?php endif; ?>
 
-		<?php if ( ! post_password_required() && comments_open() ) : ?>
+		<?php if ( ! post_password_required() && comments_open() && independent_publisher_is_not_first_post_full_content() ) : ?>
 			<span class="sep"> | </span>
 			<span class="comments-link"><?php comments_popup_link( __( 'Comment', 'independent_publisher' ), __( '1 Comment', 'independent_publisher' ), __( '% Comments', 'independent_publisher' ) ); ?></span>
 		<?php endif; ?>
 
-		<?php edit_post_link( __( 'Edit', 'independent_publisher' ),  '<span class="sep"> | </span><span class="edit-link">', '</span>' ); ?>
+		<?php $edit_link_separator = ( independent_publisher_is_not_first_post_full_content() ? '<span class="sep"> | </span>' : '' ); ?>
+		<?php edit_post_link( __( 'Edit', 'independent_publisher' ), $edit_link_separator . '<span class="edit-link">', '</span>' ); ?>
 
+		<?php if ( false === get_post_format() && independent_publisher_use_enhanced_excerpts() && independent_publisher_is_not_first_post_full_content() && ! is_sticky() ) : ?>
+			<span class="enhanced-excerpt-read-more"><a class="read-more" href="<?php the_permalink(); ?>"><?php echo __( 'Continue Reading &rarr;', 'independent_publisher' ); ?></a></span>
+		<?php endif; ?>
 	</footer>
 	<!-- .entry-meta -->
 </article><!-- #post-<?php the_ID(); ?> -->
--- inc/customizer.php
+++ inc/customizer.php
@@ -2,45 +2,86 @@
 /**
  * Independent Publisher Theme Customizer
  *
+ * Contains methods for customizing the theme customization screen.
+ *
+ * @link http://codex.wordpress.org/Theme_Customization_API
+ *
  * @package Independent Publisher
  * @since   Independent Publisher 1.0
  */
 
-/**
- * @param WP_Customize_Manager $wp_customize Theme Customizer object.
- */
-function independent_publisher_customize_register( $wp_customize ) {
+class IndependentPublisher_Customize {
 
-	$wp_customize->add_section( 'independent_publisher_general_options', array(
-		'title'    => __( 'Theme Options', 'independent_publisher' ),
-		'priority' => 130,
-	) );
+	public static function register ( $wp_customize ) {
 
-	// Show Post Word Count
-	$wp_customize->add_setting( 'independent_publisher_general_options[show_post_word_count]', array(
-		'default'    => true,
-		'type'       => 'option',
-		'capability' => 'edit_theme_options',
-	) );
-	$wp_customize->add_control( 'show_post_word_count', array(
-		'settings' => 'independent_publisher_general_options[show_post_word_count]',
-		'label'    => __( 'Show Post Word Count in Entry Meta', 'independent_publisher' ),
-		'section'  => 'independent_publisher_general_options',
-		'type'     => 'checkbox',
-	) );
+		$wp_customize->add_section( 'independent_publisher_excerpt_options', array(
+			'title'    => __( 'Excerpts', 'independent_publisher' ),
+			'priority' => 125,
+		) );
 
-	// Show Widgets on Single pages
-	$wp_customize->add_setting( 'independent_publisher_general_options[show_widgets_on_single]', array(
-		'default'    => false,
-		'type'       => 'option',
-		'capability' => 'edit_theme_options',
-	) );
-	$wp_customize->add_control( 'show_widgets_on_single', array(
-		'settings' => 'independent_publisher_general_options[show_widgets_on_single]',
-		'label'    => __( 'Show Widgets on Single Pages', 'independent_publisher' ),
-		'section'  => 'independent_publisher_general_options',
-		'type'     => 'checkbox',
-	) );
+		$wp_customize->add_section( 'independent_publisher_general_options', array(
+			'title'    => __( 'General Options', 'independent_publisher' ),
+			'priority' => 130,
+		) );
+
+		// Excerpt Options
+		$wp_customize->add_setting( 'independent_publisher_excerpt_options[excerpts]', array(
+			'default'    => '0',
+			'type'       => 'option',
+			'capability' => 'edit_theme_options',
+		) );
+		$wp_customize->add_control( 'excerpts', array(
+			'label'      => __('Post Excerpts', 'independent_publisher'),
+			'settings' => 'independent_publisher_excerpt_options[excerpts]',
+			'section' => 'independent_publisher_excerpt_options',
+			'type'    => 'radio',
+			'choices'    => array(
+				'0' => 'Disabled',
+				'1' => 'Default Excerpts',
+				'2' => 'One-Sentence Excerpts',
+			),
+		) );
+
+		// Show Full Content for First Post
+		$wp_customize->add_setting( 'independent_publisher_excerpt_options[show_full_content_first_post]', array(
+			'default'    => false,
+			'type'       => 'option',
+			'capability' => 'edit_theme_options',
+		) );
+		$wp_customize->add_control( 'show_full_content_first_post', array(
+			'settings' => 'independent_publisher_excerpt_options[show_full_content_first_post]',
+			'label'    => __( 'Always Show Full Content for First Post', 'independent_publisher' ),
+			'section'  => 'independent_publisher_excerpt_options',
+			'type'     => 'checkbox',
+		) );
+
+		// Show Post Word Count
+		$wp_customize->add_setting( 'independent_publisher_general_options[show_post_word_count]', array(
+			'default'    => true,
+			'type'       => 'option',
+			'capability' => 'edit_theme_options',
+		) );
+		$wp_customize->add_control( 'show_post_word_count', array(
+			'settings' => 'independent_publisher_general_options[show_post_word_count]',
+			'label'    => __( 'Show Post Word Count in Entry Meta', 'independent_publisher' ),
+			'section'  => 'independent_publisher_general_options',
+			'type'     => 'checkbox',
+		) );
+
+		// Show Widgets on Single pages
+		$wp_customize->add_setting( 'independent_publisher_general_options[show_widgets_on_single]', array(
+			'default'    => false,
+			'type'       => 'option',
+			'capability' => 'edit_theme_options',
+		) );
+		$wp_customize->add_control( 'show_widgets_on_single', array(
+			'settings' => 'independent_publisher_general_options[show_widgets_on_single]',
+			'label'    => __( 'Show Widgets on Single Pages', 'independent_publisher' ),
+			'section'  => 'independent_publisher_general_options',
+			'type'     => 'checkbox',
+		) );
+	}
 }
 
-add_action( 'customize_register', 'independent_publisher_customize_register' );
+// Setup the Theme Customizer settings and controls
+add_action( 'customize_register' , array( 'IndependentPublisher_Customize' , 'register' ) );
