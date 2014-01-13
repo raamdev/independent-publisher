bc35e9a0ebd736a8b727035240271e0cb7947a3b
Raam Dev <raam@raamdev.com>
2014-01-13 18:58:03 -0500

    Make JetPack Sharing CSS enhancements child-theme friendly

--- functions.php
+++ functions.php
@@ -123,10 +123,40 @@ function independent_publisher_scripts() {
 	if ( is_singular() && wp_attachment_is_image( $post->ID ) ) {
 		wp_enqueue_script( 'keyboard-image-navigation', get_template_directory_uri() . '/js/keyboard-image-navigation.js', array( 'jquery' ), '20120202' );
 	}
+
+	independent_publisher_jetpack_sharing_css();
+	independent_publisher_jetpack_sharing_label_css();
+
 }
 
 add_action( 'wp_enqueue_scripts', 'independent_publisher_scripts' );
 
+if ( ! function_exists( 'independent_publisher_jetpack_sharing_css' ) ) :
+	/**
+	 * Improves the style of JetPack Sharing Buttons when used with this theme
+	 */
+	function independent_publisher_jetpack_sharing_css() {
+		$sharedaddy_disable_resources = get_option( 'sharedaddy_disable_resources' );
+		if ( isset( $sharedaddy_disable_resources ) && $sharedaddy_disable_resources !== "1" ) {
+			wp_enqueue_style( 'independent-publisher-jetpack-sharing', get_template_directory_uri() . '/css/jetpack-sharing.css', array(), '1.0' );
+		}
+	}
+endif;
+
+if ( ! function_exists( 'independent_publisher_jetpack_sharing_label_css' ) ) :
+	/**
+	 * When the JetPack Sharing Buttons "Sharing label" is blank, this floats the sharing
+	 * buttons right instead of left and removes right padding to improve display.
+	 */
+	function independent_publisher_jetpack_sharing_label_css() {
+		$sharedaddy_options = get_option( 'sharing-options' );
+		if ( isset( $sharedaddy_options['global']['sharing_label'] ) ) {
+			$sharedaddy_disable_resources = get_option( 'sharedaddy_disable_resources' );
+			if ( trim( $sharedaddy_options['global']['sharing_label'] ) === '' && ( ! isset( $sharedaddy_disable_resources ) || $sharedaddy_disable_resources !== "1" ) )
+				wp_enqueue_style( 'independent-publisher-jetpack-sharing-label', get_template_directory_uri() . '/css/jetpack-sharing-label.css', array(), '1.0' );
+		}
+	}
+endif;
 
 /**
  * Echoes the theme's footer credits
@@ -725,18 +755,5 @@ function independent_publisher_replytocom() {
 			});
 		</script>
 	<?php
-	}
-}
-
-/**
- * When the JetPack Sharing Buttons "Sharing label" is blank, this floats the sharing
- * buttons right instead of left and removes right padding to improve display.
- */
-function independent_publisher_jetpack_sharing_buttons_css() {
-	$sharedaddy_options = get_option( 'sharing-options' );
-	if ( isset( $sharedaddy_options['global']['sharing_label'] ) ) {
-		$sharedaddy_disable_resources = get_option( 'sharedaddy_disable_resources' );
-		if ( trim( $sharedaddy_options['global']['sharing_label'] ) === '' && ( ! isset( $sharedaddy_disable_resources ) || $sharedaddy_disable_resources !== "1" ) )
-			echo '<style type="text/css">div.sharedaddy .sd-content { float: right !important; width: auto !important; }</style>';
 	}
 }
