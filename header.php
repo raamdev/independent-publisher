5d070076c64c45e6caff3476a09e193285ff6078
Raam Dev <raam@raamdev.com>
2014-01-23 16:39:22 -0500

    Properly enqueue hide-address-bar.js script

--- header.php
+++ header.php
@@ -18,7 +18,6 @@
 	<!--[if lt IE 9]>
 	<script src="<?php echo get_template_directory_uri(); ?>/js/html5.js" type="text/javascript"></script>
 	<![endif]-->
-	<script src="<?php echo get_template_directory_uri(); ?>/js/hide-address-bar.js" type="text/javascript"></script>
 	<?php wp_head(); ?>
 </head>
 
