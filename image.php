1f5431f994741e659ea4ead6696a3911abb7abc1
Raam Dev <raam@raamdev.com>
2014-01-23 17:24:21 -0500

    Improve styling on Image Attachment page

--- image.php
+++ image.php
@@ -16,14 +16,11 @@ get_header();
 
 				<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 					<header class="entry-header">
-						<h1 class="entry-title"><?php the_title(); ?></h1>
-
-						<div class="entry-meta">
+						<h2 class="entry-title-meta">
 							<?php
 							$metadata = wp_get_attachment_metadata();
-							printf( __( 'Published <span class="entry-date"><time class="entry-date" datetime="%1$s" pubdate>%2$s</time></span> at <a href="%3$s" title="Link to full-size image">%4$s &times; %5$s</a> in <a href="%6$s" title="Return to %7$s" rel="gallery">%7$s</a>', 'independent_publisher' ),
-								esc_attr( get_the_date( 'c' ) ),
-								esc_html( get_the_date() ),
+							printf( __( '"%1$s" - <a href="%2$s" title="Link to full-size image">%3$s &times; %4$s</a> in <a href="%5$s" title="Return to %6$s" rel="gallery">%6$s</a>', 'independent_publisher' ),
+								get_the_title(),
 								wp_get_attachment_url(),
 								$metadata['width'],
 								$metadata['height'],
@@ -31,16 +28,11 @@ get_header();
 								get_the_title( $post->post_parent )
 							);
 							?>
-							<?php $separator = apply_filters( 'independent_publisher_entry_meta_separator', '|' ); // @TODO Document independent_publisher_entry_meta_separator filter ?>
-							<?php edit_post_link( __( 'Edit', 'independent_publisher' ), '<span class="sep"> ' . $separator . ' </span> <span class="edit-link">', '</span>' ); ?>
-						</div>
-						<!-- .entry-meta -->
 
-						<nav id="image-navigation" class="site-navigation">
-							<span class="previous-image"><?php previous_image_link( false, __( '&larr; Previous', 'independent_publisher' ) ); ?></span>
-							<span class="next-image"><?php next_image_link( false, __( 'Next &rarr;', 'independent_publisher' ) ); ?></span>
-						</nav>
-						<!-- #image-navigation -->
+							<?php do_action( 'independent_publisher_entry_title_meta', $separator = ' | ' ); ?>
+						</h2>
+
+						<!-- .entry-meta -->
 					</header>
 					<!-- .entry-header -->
 
@@ -90,28 +82,30 @@ get_header();
 						<!-- .entry-attachment -->
 
 						<?php the_content(); ?>
-						<?php wp_link_pages( array( 'before' => '<div class="page-links-next-prev">', 'after' => '</div>', 'nextpagelink' => __( '<button class="next-page-nav">Next page &rarr;</button>', 'independent_publisher' ), 'previouspagelink' => __( '<button class="previous-page-nav">&larr; Previous page</button>', 'independent_publisher' ), 'next_or_number' => 'next' ) ); ?>
-						<?php wp_link_pages( array( 'before' => '<div class="page-links">' . __( 'Pages:', 'independent_publisher' ), 'after' => '</div>' ) ); ?>
+
+						<nav id="image-navigation" class="site-navigation">
+							<span class="previous-image"><?php previous_image_link( false, __( '&larr; Previous image', 'independent_publisher' ) ); ?></span>
+							<span class="next-image"><?php next_image_link( false, __( 'Next image &rarr;', 'independent_publisher' ) ); ?></span>
+						</nav>
+						<!-- #image-navigation -->
 
 					</div>
 					<!-- .entry-content -->
 
+					<?php independent_publisher_posted_author_bottom_card() ?>
+
 					<footer class="entry-meta">
-						<?php if ( comments_open() && pings_open() ) : // Comments and trackbacks open ?>
-							<?php printf( __( '<a class="comment-link" href="#respond" title="Post a comment">Post a comment</a> or leave a trackback: <a class="trackback-link" href="%s" title="Trackback URL for your post" rel="trackback">Trackback URL</a>.', 'independent_publisher' ), get_trackback_url() ); ?>
-						<?php elseif ( ! comments_open() && pings_open() ) : // Only trackbacks open ?>
-							<?php printf( __( 'Comments are closed, but you can leave a trackback: <a class="trackback-link" href="%s" title="Trackback URL for your post" rel="trackback">Trackback URL</a>.', 'independent_publisher' ), get_trackback_url() ); ?>
-						<?php
-						elseif ( comments_open() && ! pings_open() ) : // Only comments open
-							?>
-							<?php _e( 'Trackbacks are closed, but you can <a class="comment-link" href="#respond" title="Post a comment">post a comment</a>.', 'independent_publisher' ); ?>
-						<?php
-						elseif ( ! comments_open() && ! pings_open() ) : // Comments and trackbacks closed
-							?>
-							<?php _e( 'Both comments and trackbacks are currently closed.', 'independent_publisher' ); ?>
+						<?php do_action( 'independent_publisher_entry_meta_top' ); // @TODO Document independent_publisher_entry_meta_top action ?>
+
+						<?php if ( comments_open() ) : ?>
+							<div id="share-comment-button">
+								<button><i class="share-comment-icon"></i>Share a comment</button>
+							</div>
 						<?php endif; ?>
-						<?php edit_post_link( __( 'Edit', 'independent_publisher' ), ' <span class="edit-link">', '</span>' ); ?>
+
+						<?php edit_post_link( __( 'Edit', 'independent_publisher' ), '<span class="edit-link">', '</span>' ); ?>
 					</footer>
+
 					<!-- .entry-meta -->
 				</article><!-- #post-<?php the_ID(); ?> -->
 
