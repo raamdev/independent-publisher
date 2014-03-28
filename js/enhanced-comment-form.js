0539ffea39026fb51d0ae69cfb1f04e1d75d9405
Raam Dev <raam@raamdev.com>
2014-03-28 12:54:48 -0400

    Fix comment form bugs when using comment reply links. See #68.

--- js/enhanced-comment-form.js
+++ js/enhanced-comment-form.js
@@ -4,14 +4,34 @@
 
 jQuery(document).ready(function () {
 
+	/* Checks if this is a link to reply directly to a comment */
+	function is_replytocom() {
+		var field = 'replytocom';
+		var url = window.location.href;
+		if(url.indexOf('?' + field + '=') != -1 || url.indexOf('&' + field + '=') != -1 )
+			return true;
+		else
+			return false;
+	}
+
 	var identifier = window.location.hash;
 
 	if (identifier === "#respond") {
 		jQuery('#respond').show();
-		jQuery('#share-comment-button').hide();
 		jQuery('#share-comment-button-bottom').show();
 		jQuery('.comment-form-reply-title').hide();
-	} else {
+
+		if(is_replytocom()) { // If this is a link to reply directly to a comment
+			jQuery('#commentform-top').hide();
+			jQuery('#commentform-bottom').hide();
+			jQuery('#share-comment-button').show();
+		} else {  // Otherwise, load the normal comment reply form
+			jQuery('#commentform-top').show();
+			jQuery('#commentform-top').append(jQuery('#respond'));
+			jQuery('#commentform-bottom').hide();
+		}
+
+	} else { // Set the default state (forms hidden, buttons showing)
 		jQuery('#respond').hide();
 		jQuery('#share-comment-button').show();
 		jQuery('#share-comment-button-bottom').show();
@@ -21,7 +41,7 @@ jQuery(document).ready(function () {
 
 	/* Share Comment Button (Top) */
 
-	jQuery('#share-comment-button').live('click', function (event) {
+	jQuery( document ).on('click', '#share-comment-button', function (event) {
 		jQuery('#commentform-top').show();
 		jQuery('#commentform-bottom').hide();
 		jQuery('#cancel-comment-reply-link').click();
@@ -32,11 +52,13 @@ jQuery(document).ready(function () {
 		jQuery('.comment-form-reply-title').hide();
 		jQuery('#main-reply-title').show();
 		jQuery('#comment').focus();
+		if(is_replytocom())
+			jQuery('#reply-title').hide();
 	});
 
 	/* Share Comment Button (Bottom)*/
 
-	jQuery('#share-comment-button-bottom').live('click', function (event) {
+	jQuery( document ).on('click', '#share-comment-button-bottom', function (event) {
 		jQuery('#commentform-bottom').show();
 		jQuery('#commentform-top').hide();
 		jQuery('#cancel-comment-reply-link').click();
@@ -47,20 +69,28 @@ jQuery(document).ready(function () {
 		jQuery('.comment-form-reply-title').hide();
 		jQuery('#main-reply-title').show();
 		jQuery('#comment').focus();
+		if(is_replytocom())
+			jQuery('#reply-title').hide();
 	});
 
-	jQuery('.comment-reply-link').live('click', function (event) {
+	jQuery( document ).on('click', '.comment-reply-link', function (event) {
 		jQuery('#respond').show();
 		jQuery('#share-comment-button').show();
 		jQuery('#share-comment-button-bottom').show();
 		jQuery('.comment-form-reply-title').show();
 		jQuery('#main-reply-title').hide();
 		jQuery('#comment').focus();
+		jQuery('#commentform-top').hide();
+		jQuery('#commentform-bottom').hide();
+		if(is_replytocom())
+			jQuery('#reply-title').hide();
 	});
 
-	jQuery('#cancel-comment-reply-link').live('click', function (event) {
+	jQuery( document ).on('click', '#cancel-comment-reply-link', function (event) {
 		jQuery('#respond').hide();
 		jQuery('#share-comment-button').show();
+		if(is_replytocom())
+			jQuery('#reply-title').hide();
 	});
 
 });
