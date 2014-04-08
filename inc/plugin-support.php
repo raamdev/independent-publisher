6995de13bc52710e5895025ebfa358183b16d574
Raam Dev <raam@raamdev.com>
2014-04-08 17:12:11 -0400

    Check if Jetpack Sharing module is active before enqueuing enhancement CSS

--- inc/plugin-support.php
+++ inc/plugin-support.php
@@ -22,7 +22,7 @@ if ( ! function_exists( 'independent_publisher_jetpack_sharing_css' ) ) :
 	 */
 	function independent_publisher_jetpack_sharing_css() {
 		$sharedaddy_disable_resources = get_option( 'sharedaddy_disable_resources' );
-		if ( isset( $sharedaddy_disable_resources ) && $sharedaddy_disable_resources !== "1" ) {
+		if ( class_exists( 'Jetpack' ) && Jetpack::is_module_active( 'sharedaddy' ) && isset( $sharedaddy_disable_resources ) && $sharedaddy_disable_resources !== "1" ) {
 			wp_enqueue_style( 'independent-publisher-jetpack-sharing-enhancements', get_template_directory_uri() . '/css/jetpack-sharing-enhancements.css', array(), '1.0' );
 		}
 	}
@@ -35,7 +35,7 @@ if ( ! function_exists( 'independent_publisher_jetpack_sharing_label_css' ) ) :
 	 */
 	function independent_publisher_jetpack_sharing_label_css() {
 		$sharedaddy_options = get_option( 'sharing-options' );
-		if ( isset( $sharedaddy_options['global']['sharing_label'] ) ) {
+		if ( class_exists( 'Jetpack' ) && Jetpack::is_module_active( 'sharedaddy' ) && isset( $sharedaddy_options['global']['sharing_label'] ) ) {
 			$sharedaddy_disable_resources = get_option( 'sharedaddy_disable_resources' );
 			if ( trim( $sharedaddy_options['global']['sharing_label'] ) === '' && $sharedaddy_options['global']['sharing_label'] !== false && ( ! isset( $sharedaddy_disable_resources ) || $sharedaddy_disable_resources !== "1" ) ) {
 				wp_enqueue_style( 'independent-publisher-jetpack-sharing-label-enhancements', get_template_directory_uri() . '/css/jetpack-sharing-label-enhancements.css', array(), '1.0' );
