9cd38724f64d7576cbee617ede0d942c411b6a8d
Raam Dev <raam@raamdev.com>
2014-08-13 00:14:44 -0400

    Update Genericons to v3.1. Fixes #128.

--- functions.php
+++ functions.php
@@ -173,7 +173,7 @@ add_action( 'widgets_init', 'independent_publisher_widgets_init' );
 function independent_publisher_scripts() {
 	global $post;
 
-	wp_enqueue_style( 'genericons', get_template_directory_uri() . '/fonts/genericons/genericons.css', array(), '3.0.3' );
+	wp_enqueue_style( 'genericons', get_template_directory_uri() . '/fonts/genericons/genericons.css', array(), '3.1' );
 
 	wp_enqueue_script( 'independent-publisher-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
 
